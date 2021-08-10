eebe9be6d1959a9d9e395da8120f92e5eb7da3a3
Dorien Reynders <dorien.reynders@hotmail.com>
2021-08-10 15:55:06 +0200

    with some CSS styling" "

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,6 +42,6 @@ class TasksController < ApplicationController
   private
 
   def params_tasks
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
